0500d68a26399260a202220a96eef19e4ae9e621
Max Gorbunov <shizow@gmail.com>
2013-01-24 21:20:50 -0800

    Added event propagation to all clients

--- public/javascripts/sockets.js
+++ public/javascripts/sockets.js
@@ -1,4 +1,4 @@
-var wsUri = "ws://echo.websocket.org/";
+var wsUri = "ws://172.24.38.8:9000/sockets";
 var output;
 
 
@@ -11,7 +11,7 @@ function testWebSocket() { websocket = new WebSocket(wsUri);
 	websocket.onerror = function(evt) { onError(evt) }; } 
 	function onOpen(evt) { writeToScreen("CONNECTED"); doSend("WebSocket rocks"); }
 	function onClose(evt) { writeToScreen("DISCONNECTED"); } 
-	function onMessage(evt) { writeToScreen('<span style="color: blue;">RESPONSE: ' + evt.data+'</span>'); websocket.close(); }
+	function onMessage(evt) { writeToScreen('<span style="color: blue;">RESPONSE: ' + evt.data+'</span>'); }
 	function onError(evt) { writeToScreen('<span style="color: red;">ERROR:</span> ' + evt.data); }
 	function doSend(message) { writeToScreen("SENT: " + message);  websocket.send(message); }
 	function writeToScreen(message) { var pre = document.createElement("p"); pre.style.wordWrap = "break-word"; pre.innerHTML = message; output.appendChild(pre); }
