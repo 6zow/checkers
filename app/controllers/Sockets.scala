dad0814e4e5c73fd17364629777a5ae398c25235
Max Gorbunov <shizow@gmail.com>
2013-01-27 12:09:41 -0800

    Created Game class out of a singleton to allow multiple games

--- app/controllers/Sockets.scala
+++ app/controllers/Sockets.scala
@@ -9,8 +9,7 @@ import java.util.concurrent.{LinkedBlockingQueue, BlockingQueue}
 import models.{User, Point}
 import models.checkers.Board
 
-object Sockets {
-
+class Game() {
   import Point.PointFormat
 
   @volatile
@@ -47,62 +46,85 @@ object Sockets {
 
   def boardRef(p: Point) = (p.x + Char.char2int('A') - 1).toChar + "" + p.y.toInt
 
+
+  // Communication part
+
+  def connectUser: User = {
+    val user = User((1 to 1000).dropWhile(id => users.exists(_.id == id)).head)
+
+    users = user :: users
+    queues = queues + (user -> new LinkedBlockingQueue[JsValue]())
+    user
+  }
+
+  def processMessage(s: JsValue)(implicit user: User) {
+    println(s"Received: $s")
+    def movable(user: User) =
+      board.movablePieces.filter(_.user == user).map(p => p.id -> board.availableMoves(p).map(boardRef)).toMap
+    (s \ "action").asOpt[String] match {
+      case Some("join") =>
+        send(Json.obj("msg" -> s"welcome $user"))
+        sendOther(Json.obj("msg" -> s"$user joined"))
+        board.pieces.foreach {
+          case (id, piece) => send(Json.obj("action" -> "moved", "id" -> id, "pos" -> piece.position))
+        }
+        send(Json.obj("movable" -> Json.toJson(movable(user))))
+      case Some("moved") =>
+        val p = (s \ "pos").as[Point]
+        val id = (s \ "id").as[String]
+        val piece = board.pieces(id)
+        val newPiece = p.constraint.flatMap(piece.move(_))
+        val pnew = newPiece match {
+          case Some(newp) =>
+            board.update(newp, piece => broadcast(Json.obj("action" -> "removed", "id" -> piece.id)))
+            sendAll(user => Json.obj("movable" -> Json.toJson(movable(user))))
+            newp.position
+          case None =>
+            piece.position
+        }
+        println(s"$p -> $pnew")
+        broadcast(Json.obj("action" -> "moved", "id" -> id, "pos" -> pnew))
+      case _ =>
+        broadcast(s)
+    }
+  }
+
+  def disconnectUser(implicit user: User) {
+    println("Disconnected")
+    send(Json.obj())
+  }
+
+  def getMessageForUser(implicit user: User): Future[Option[JsValue]] = {
+    Future[Option[JsValue]] {
+      println(s"awaiting for $user")
+      queues(user).take match {
+        case JsObject(Nil) => {
+          users = users.filterNot(_ == user)
+          queues = queues - user
+          None
+        }
+        case msg => Some(msg)
+      }
+    }
+  }
+}
+
+object Sockets {
+  val game = new Game
+
   def index = WebSocket.using[JsValue] {
     request =>
-      @volatile
-      implicit var user = User((1 to 1000).dropWhile(id => users.exists(_.id == id)).head)
-
-      users = user :: users
-      queues = queues + (user -> new LinkedBlockingQueue[JsValue]())
+      implicit val user: User = game.connectUser
       // Log events to the console
       val in = Iteratee.foreach[JsValue] {
         s =>
-          println(s"Received: $s")
-          def movable(user: User) =
-            board.movablePieces.filter(_.user == user).map(p => p.id -> board.availableMoves(p).map(boardRef)).toMap
-          (s \ "action").asOpt[String] match {
-            case Some("join") =>
-              send(Json.obj("msg" -> s"welcome $user"))
-              sendOther(Json.obj("msg" -> s"$user joined"))
-              board.pieces.foreach {
-                case (id, piece) => send(Json.obj("action" -> "moved", "id" -> id, "pos" -> piece.position))
-              }
-              send(Json.obj("movable" -> Json.toJson(movable(user))))
-            case Some("moved") =>
-              val p = (s \ "pos").as[Point]
-              val id = (s \ "id").as[String]
-              val piece = board.pieces(id)
-              val newPiece = p.constraint.flatMap(piece.move(_))
-              val pnew = newPiece match {
-                case Some(newp) =>
-                  board.update(newp, piece => broadcast(Json.obj("action" -> "removed", "id" -> piece.id)))
-                  sendAll(user => Json.obj("movable" -> Json.toJson(movable(user))))
-                  newp.position
-                case None =>
-                  piece.position
-              }
-              println(s"$p -> $pnew")
-              broadcast(Json.obj("action" -> "moved", "id" -> id, "pos" -> pnew))
-            case _ =>
-              broadcast(s)
-          }
+          game.processMessage(s)
       }.mapDone {
         _ =>
-          println("Disconnected")
-          send(Json.obj())
+          game.disconnectUser
       }
 
-      val out = Enumerator.generateM(Future[Option[JsValue]] {
-        println(s"awaiting for $user")
-        queues(user).take match {
-          case JsObject(Nil) => {
-            users = users.filterNot(_ == user)
-            queues = queues - user
-            None
-          }
-          case msg => Some(msg)
-        }
-      })
+      val out = Enumerator.generateM(game.getMessageForUser)
 
       (in, out)
   }
